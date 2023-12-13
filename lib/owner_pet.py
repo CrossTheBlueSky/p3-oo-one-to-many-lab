3efbdd9175415725081099c2da5f1aa804023694
Derek Mercedes <mercedes.derek@gmail.com>
2023-12-13 14:23:10 -0700

    I used a lambda

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,30 @@
 class Pet:
-    pass
+
+    PET_TYPES = ['dog', 'cat', 'rodent', 'bird', 'reptile', 'exotic']
+    all = []
+
+    def __init__(self, name, pet_type, owner = None):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)
+
+        if pet_type not in Pet.PET_TYPES:
+            raise(Exception)
 
 class Owner:
-    pass
+
+    def __init__(self, name, pets = None):
+        self.name = name
+        self.pet_arr = pets
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+    
+    def add_pet(self, pet):
+        if type(pet) != Pet:
+            raise TypeError("Not a pet")
+        else:
+            pet.owner = self
+    def get_sorted_pets(self):
+                return sorted(self.pets(), key = lambda pet: pet.name)
